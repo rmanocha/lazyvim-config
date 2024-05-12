a30b832f7a44e8685cc47e0118c0777bee3c3bfc
Rishabh Manocha <rmanocha@gmail.com>
2024-05-12 05:54:25 -0700

    setup dependencies and configs

--- lua/plugins/linear-nvim.lua
+++ lua/plugins/linear-nvim.lua
@@ -1,5 +1,12 @@
 return {
   {
     dir = "~/dev/linear-nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("linear-nvim").setup()
+    end,
   },
 }

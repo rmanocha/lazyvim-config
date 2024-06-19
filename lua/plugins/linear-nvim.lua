29a088b8288bb586b17b750198e69458b537b75d
Rishabh Manocha <rmanocha@gmail.com>
2024-06-19 09:15:49 +0800

    some updates and setup default labels for linear-nvim

--- lua/plugins/linear-nvim.lua
+++ lua/plugins/linear-nvim.lua
@@ -9,6 +9,7 @@ return {
       require("linear-nvim").setup({
         issue_regex = "[kK][aA][rR]%-%d+",
         issue_fields = { "title", "description", "url", "identifier", "branchName" },
+        default_label_ids = { "aec87c80-3939-4006-b972-0b3d85cd4097" },
       })
       -- require("linear-nvim").setup()
     end,

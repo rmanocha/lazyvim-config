fdce633a1ab5cad7ed56769b395f46245ae94cd8
Rishabh Manocha <rmanocha@gmail.com>
2025-02-03 08:06:39 +0800

    configure rust stuff too

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,5 +9,7 @@ return {
     table.insert(opts.ensure_installed, "prettier")
     table.insert(opts.ensure_installed, "typescript-language-server")
     table.insert(opts.ensure_installed, "eslint_d")
+    -- rust stuff
+    table.insert(opts.ensure_installed, "rust-analyzer")
   end,
 }
